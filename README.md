c24ed2d40e0e8a5a16d594c603845b87d7ff2b03
Anastasia Dementeva <nastya.dementeva.2016@mail.ru>
2016-10-04 12:13:10 +0300

    version for desktop 960px and >

---
+++ README.md
@@ -0,0 +1,4 @@
+1. git clone https://github.com/Nastya2/page_shop.git site
+2. cd site
+3. npm i
+4. gulp 
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+/* --------- plugins --------- */
+
+var
+	gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+    nested = require('postcss-nested'),
+    stylelint = require('stylelint'),
+    config = require('stylelint-config-standard'),
+    reporter = require('postcss-browser-reporter'),
+    jade = require('gulp-jade'),
+    browserSync = require('browser-sync'),
+    plumber = require('gulp-plumber'),
+    del = require('del'), 
+    imagemin = require('gulp-imagemin'), 
+    cache = require('gulp-cache'),
+    spritesmith = require("gulp.spritesmith");
+    
+    
+	
+  
+/*----------------post-css------------------*/
+
+gulp.task('css', function () {
+    var processors = [
+        stylelint({config}),
+        autoprefixer({browsers: ['last 3 version']}),
+        nested,
+        
+        reporter({
+            'selector' : 'body:before'
+        }),
+    ];
+    return gulp.src('app/post_css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('app/css/'));
+});
+
+/*----------------jade------------------*/
+gulp.task('jade', function() {
+    return gulp.src('app/jade/index.jade')
+     .pipe(plumber())
+    .pipe(jade({
+        pretty: true
+    }))
+    .pipe(gulp.dest('app'))
+});
+ 
+/*----------------browser-sync------------------*/
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server:'app'
+    });
+
+browserSync.watch('app/**/*.*').on('change',browserSync.reload);
+});
+
+/*-------------------sprite-------------------*/
+gulp.task('sprites', function() {
+  var spriteData = gulp.src('app/img/sprite/*.png').pipe(
+    spritesmith({
+      imgName: '../img/sprite.png',
+      cssName: 'sprite.css',
+      padding: 70,
+    })
+  );
+  spriteData.css.pipe(gulp.dest('app/css'));
+  return spriteData.img.pipe(gulp.dest('app/img'));
+});
+/*----------------watch------------------*/
+gulp.task('watch',['css','jade','browser-sync','sprites'],function(){
+    gulp.watch('app/post_css/*.css',['css']);
+    gulp.watch('app/jade/*.jade',['jade']);
+    gulp.watch('app/*.html');
+    gulp.watch('app/js/**/*.js');
+    gulp.watch('app/img/sprite/**/*.png')['sprites'];
+});
+/*----------------images------------------*/
+gulp.task('default', () =>
+    gulp.src('app/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+
+/*----------------in production------------------*/
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('build', ['clean', 'jade', 'css','imagemin',], function() {
+
+    var buildCss = gulp.src('app/post_css/**/*')
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') 
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') 
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') 
+    .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+});
+
+gulp.task('default', ['watch']);
